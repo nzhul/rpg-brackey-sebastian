e7629f77b1236ca0a3863eb8b548e5b90f2423dd
nzhul <dobromirivanov1@gmail.com>
2018-05-23 23:03:17 +0300

    up

---
+++ RPG/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(PlayerMotor))]
+public class PlayerController : MonoBehaviour
+{
+
+    public LayerMask movementMask;
+    Camera cam;
+    PlayerMotor motor;
+
+    private void Start()
+    {
+        cam = Camera.main;
+        motor = GetComponent<PlayerMotor>();
+    }
+
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Ray ray = cam.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+
+            if (Physics.Raycast(ray, out hit, 100f, movementMask))
+            {
+                motor.MoveToPoint(hit.point);
+
+                // move our player to what we hit
+
+                // stop focusing any objects
+            }
+        }
+        if (Input.GetMouseButtonDown(1))
+        {
+            Ray ray = cam.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+
+            if (Physics.Raycast(ray, out hit, 100))
+            {
+                // check if we hit an interactable
+                // if we did - set it as our focus
+            }
+        }
+    }
+
+}
---
+++ RPG/Assets/Scripts/PlayerMotor.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using UnityEngine.AI;
+
+[RequireComponent(typeof(NavMeshAgent))]
+public class PlayerMotor : MonoBehaviour
+{
+
+    NavMeshAgent agent;
+
+    // Use this for initialization
+    void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void MoveToPoint(Vector3 point)
+    {
+        agent.SetDestination(point);
+    }
+}
