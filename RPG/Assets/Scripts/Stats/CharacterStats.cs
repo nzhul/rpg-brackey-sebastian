8c46987360922c7949e211020d4f01e3e0042d28
nzhul <dobromirivanov1@gmail.com>
2018-05-31 00:13:58 +0300

    update

---
+++ RPG/Assets/Scripts/Stats/CharacterStats.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+
+public class CharacterStats : MonoBehaviour {
+
+    public int maxHealth = 100;
+    public int CurrentHealth { get; private set; }
+
+    public Stat damage;
+    public Stat armor;
+
+    private void Awake()
+    {
+        CurrentHealth = maxHealth;
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.T))
+        {
+            TakeDamage(10);
+        }
+    }
+
+    public void TakeDamage(int damage)
+    {
+        damage -= armor.GetValue();
+        damage = Mathf.Clamp(damage, 0, int.MaxValue);
+
+        CurrentHealth -= damage;
+        Debug.Log(transform.name + " takes " + damage + " damage.");
+
+        if (CurrentHealth <= 0)
+        {
+            Die();
+        }
+    }
+
+    public virtual void Die()
+    {
+        // Die in some way
+        // This method is meant to be overwritten
+        Debug.Log(transform.name + " died.");
+    }
+}
